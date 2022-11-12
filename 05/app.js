546b72f2d90eee8666756eb8e34c0ca180793748
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-11-12 17:53:09 +0100

    task 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(function(number) {
+    return number % 2 === 0;
+});
+
+
+const sum = evenNumbers.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+})
+
+console.log(evenNumbers);
+console.log(sum);
