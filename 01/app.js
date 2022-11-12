6631d0b430efb6d57179052c703078fbc5f653d7
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-11-12 15:29:51 +0100

    task 01

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+
+const users = new Array('Anna Kowalska', 'Maria Sosnowska', 'Barbara Kwiatkowska', 'Filip Makowski', 'Andrzej Małecki');
+
+console.log('Użytkownik pierwszy: ' + users[0]);
+console.log('Użytkownik drugi: ' + users[2]);
+console.log('Użytkownik piąty: ' + users[4]);
+
+console.log('Tablica zawiera ' + users.length + ' elementów');
