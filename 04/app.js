07fae3dc504923eb3b7556ffc840a58ef88fe241
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-11-12 17:26:43 +0100

    task 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 const years = [1980, 1934, 2002, 2019];
+let today = new Date();
+
+const pastYears = years.map(function(element) {
+    return today.getFullYear() - element;
+});
+
+console.log(pastYears);
